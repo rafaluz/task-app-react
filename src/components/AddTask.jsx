41a3fb540a8821b8dd5f7e842d506378d75f80a5
Rafael Luz Araújo <rafa_luz_araujo@hotmail.com>
2025-06-24 12:27:04 -0300

    estilização do componente addTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,11 @@
 function AddTask() {
-    return <h1>Add Task</h1>
+    return (
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite o texto da tarefa" className="border border-slate-400 outline-slate-500 px-4 py-2 rounded-md" />
+            <input type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-400 outline-slate-500 px-4 py-2 rounded-md" />
+            <button className="bg-slate-400 text-white p-2 rounded-md">Adicionar</button>
+        </div>
+    )
 }
 
 export default AddTask;
